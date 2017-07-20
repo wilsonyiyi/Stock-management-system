545a15623f0c1191e1d6844c166dc296b45976dc
wangweiwei <will_workhard@163.com>
2017-07-20 11:24:21 +0800

    管道：根据字段匹配过滤

---
+++ stock/src/app/stock/stock-filter.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {Stock} from "./stock.service";
+
+@Pipe({
+  name: 'stockFilter'
+})
+export class StockFilterPipe implements PipeTransform {
+
+  transform(list: Stock[], field: string, search: string): any {
+    if (!field || !search) return list;
+    return list.filter(item => {
+      console.log("item: ", item)
+      let itemFieldValue = item[field];
+      return itemFieldValue.indexOf(search) > -1
+	  })
+  }
+
+}
