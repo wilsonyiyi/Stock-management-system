c28a3717fb9bbbfcd668e283e85900d3612eb252
wangweiwei <will_workhard@163.com>
2018-01-24 00:09:57 +0800

    add something

---
+++ test/testOutput/src/app/father/father.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-father',
+  template: `
+	  
+  `,
+  styleUrls: ['./father.component.css']
+})
+export class FatherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
